3e7bef8e532fe5edddd97d0ef93d828b42d4f1d8
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-05-26 23:45:44 +0500

    chore: bump version and remove Add to Cart tracking snippet from templates

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'description': 'Odoo eCommerce Integration with Klaviyo.',
     'author': 'FPG',
     'category': 'Marketing',
-    'version': '19.0.1.0.2',
+    'version': '19.0.1.0.3',
     'depends': [
         'website_sale',
         'fpg_odoo_klaviyo_key'
