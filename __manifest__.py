ccc904a77c203176f4168d9f5b0a9991c39a59be
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-06-02 22:35:19 +0500

    chore: bump module version to 19.0.1.1.3

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'description': 'Odoo eCommerce Integration with Klaviyo.',
     'author': 'FPG',
     'category': 'Marketing',
-    'version': '19.0.1.1.2',
+    'version': '19.0.1.1.3',
     'depends': [
         'website_sale',
         'fpg_odoo_klaviyo_key',
