2d2f404b43348176bc2715989cfbc17d89ec0f0c
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-06-13 00:31:12 +0500

    chore: increment manifest version to 19.0.1.1.21

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'description': 'Odoo eCommerce Integration with Klaviyo.',
     'author': 'FPG',
     'category': 'Marketing',
-    'version': '19.0.1.1.20',
+    'version': '19.0.1.1.21',
     'depends': [
         'website_sale',
         'fpg_odoo_klaviyo_key',
